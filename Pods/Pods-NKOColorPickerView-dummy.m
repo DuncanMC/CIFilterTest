b44bd2e0245df67663c1aa6747c2242af204549d
Duncan Champney <dchampney@wareto.com>
2014-03-28 15:32:25 -0400

    added Pods folder to project

---
+++ Pods/Pods-NKOColorPickerView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NKOColorPickerView : NSObject
+@end
+@implementation PodsDummy_Pods_NKOColorPickerView
+@end
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// NKOColorPickerView
+#define COCOAPODS_POD_AVAILABLE_NKOColorPickerView
+#define COCOAPODS_VERSION_MAJOR_NKOColorPickerView 0
+#define COCOAPODS_VERSION_MINOR_NKOColorPickerView 2
+#define COCOAPODS_VERSION_PATCH_NKOColorPickerView 0
+
